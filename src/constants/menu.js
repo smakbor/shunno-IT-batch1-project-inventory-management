e6a0542080550b2f6a54e8c5416307093f6f489e
Neyamul Omy <neyamul.omy@gmail.com>
2023-04-26 20:48:40 +0600

    products and suppliers added in sidemenu

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -26,6 +26,58 @@ const MENU_ITEMS = () => {
         },
         { key: 'Contacts', label: t('contacts'), isTitle: true },
         {
+            key: 'Products',
+            label: t('products'),
+            isTitle: false,
+            icon: 'dripicons-weight',
+            children: [
+                {
+                    key: 'products',
+                    label: t('products'),
+                    url: '/products',
+                    parentKey: 'Products',
+                },
+                {
+                    key: 'category',
+                    label: t('category'),
+                    url: '/category',
+                    parentKey: 'Products',
+                },
+                {
+                    key: 'unit',
+                    label: t('unit'),
+                    url: '/unit',
+                    parentKey: 'Products',
+                },
+                {
+                    key: 'manufacturer',
+                    label: t('manufacturer'),
+                    url: '/manufacturer',
+                    parentKey: 'Products',
+                },
+                {
+                    key: 'warranty',
+                    label: t('warranty'),
+                    url: '/warranty',
+                    parentKey: 'Products',
+                },
+                {
+                    key: 'stock',
+                    label: t('stock'),
+                    url: '/stock',
+                    parentKey: 'Products',
+                },
+            ],
+        },
+        {
+            key: 'Suppliers',
+            label: t('suppliers'),
+            isTitle: false,
+            icon: 'uil-briefcase',
+            url: '/suppliers'
+
+        },
+        {
             key: 'Customers',
             label: t('customers'),
             isTitle: false,
