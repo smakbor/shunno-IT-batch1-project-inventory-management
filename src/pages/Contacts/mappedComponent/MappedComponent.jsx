9f36d2b32032821b412c8c7b2222a3b684569905
smakbor <smakbar3965@gmail.com>
2023-06-15 16:02:41 +0600

    staff create update completed

--- src/pages/Contacts/mappedComponent/MappedComponent.jsx
+++ src/pages/Contacts/mappedComponent/MappedComponent.jsx
@@ -33,22 +33,43 @@ const MappedComponent = ({
         <form onSubmit={handleSubmit(onSubmit)} className={'formClass'} noValidate>
             <Row>
                 {inputField.map((item) => {
-                    return (
-                        <Col className={item.col}>
-                            <FormInput
-                                label={item.label}
-                                type={item.type}
-                                name={item.name}
-                                placeholder={item.placeholder}
-                                containerClass={item.containerClass}
-                                required={item.required}
-                                register={register}
-                                errors={errors}
-                                option={item.options}
-                                control={control}
-                                setValue={item.setValue}></FormInput>
-                        </Col>
-                    );
+                    if (editData && item.isVisible == false) {
+                        return (
+                            <Col className="d-none">
+                                <FormInput
+                                    label={item.label}
+                                    type={item.type}
+                                    name={item.name}
+                                    placeholder={item.placeholder}
+                                    containerClass={item.containerClass}
+                                    required={item.required}
+                                    register={register}
+                                    errors={errors}
+                                    option={item.options}
+                                    control={control}
+                                    watchValue={item.watchValue}
+                                    setValue={setValue}></FormInput>
+                            </Col>
+                        );
+                    } else {
+                        return (
+                            <Col className={item.col}>
+                                <FormInput
+                                    label={item.label}
+                                    type={item.type}
+                                    name={item.name}
+                                    placeholder={item.placeholder}
+                                    containerClass={item.containerClass}
+                                    required={item.required}
+                                    register={register}
+                                    errors={errors}
+                                    option={item.options}
+                                    control={control}
+                                    watchValue={item.watchValue}
+                                    setValue={setValue}></FormInput>
+                            </Col>
+                        );
+                    }
                 })}
 
                 <div className="mt-3 text-end">
